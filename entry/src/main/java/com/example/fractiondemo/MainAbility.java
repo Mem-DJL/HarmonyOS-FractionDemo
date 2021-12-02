7eeafd82b5c10dda3304df0b22955e1deb9a0449
DuJinLong <2540187660@qq.com>
2021-12-02 09:49:49 +0800

    initialize repository

---
+++ entry/src/main/java/com/example/fractiondemo/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.fractiondemo;
+
+import com.example.fractiondemo.slice.MainAbilitySlice;
+import ohos.aafwk.ability.fraction.FractionAbility;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends FractionAbility {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
---
+++ entry/src/test/java/com/example/fractiondemo/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.fractiondemo;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
